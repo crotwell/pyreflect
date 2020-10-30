169a1db3951b89d2eb6c6e858b77a0f8995b3330
Philip Crotwell <crotwell@seis.sc.edu>
2020-10-30 11:18:03 -0400

    use find_packages

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-from distutils.core import setup
+import setuptools
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(
+setuptools.setup(
     name = 'pyreflect',
-    packages = ['pyreflect'],
+    packages=setuptools.find_packages(),
     version = '0.0.1',
     description = "Python codes to work with George Randall's synthetic seismogram code",
     author='Philip Crotwell',
     author_email='crotwell@seis.sc.edu',
     url='https://github.com/crotwell/pyreflect',
     classifiers=["Development Status :: 3 - Alpha",
-                 "Programming Language :: Python",
                  "Programming Language :: Python :: 3",
                  "Intended Audience :: Science/Research",
                  "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
