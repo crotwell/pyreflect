7645434b5c59660373c305ea5145775934d4b97e
Philip Crotwell <crotwell@seis.sc.edu>
2021-04-21 14:31:11 -0400

    create fake stations for dist from model

--- pyreflect/stationmetadata.py
+++ pyreflect/stationmetadata.py
@@ -2,6 +2,11 @@ from datetime import datetime, tzinfo, timezone
 from .momenttensor import moment_scale_factor
 from .specfile import AMP_STYLE_DISP, AMP_STYLE_VEL
 
+#import .earthmodel
+DIST_SINGLE=1
+DIST_REGULAR=0
+DIST_IRREGULAR=-1
+
 emptyStationXML = """
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <FDSNStationXML xmlns="http://www.fdsn.org/xml/station/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.fdsn.org/xml/station/1 http://www.fdsn.org/xml/station/fdsn-station-1.1.xsd" schemaVersion="1.1">
@@ -10,9 +15,9 @@ emptyStationXML = """
     <Created>{now}</Created>
     <Network code="{netcode}" startDate="{start}" >
         <Station code="{stacode}" startDate="{start}">
-            <Latitude>36.9488</Latitude>
-            <Longitude>-116.2495</Longitude>
-            <Elevation>1600.0</Elevation>
+            <Latitude>{lat}</Latitude>
+            <Longitude>{lon}</Longitude>
+            <Elevation>0.0</Elevation>
             <Site>
                <Name>{sitename}</Name>
             </Site>
@@ -115,3 +120,49 @@ def createMetadata(network, station, loccode, bandcode, gaincode, model, scalar_
       "sps": model.frequency['nyquist']
     }
     return emptyStationXML.format(**data).strip()
+
+
+def createFakeMetadata(model, loccode, bandcode, gaincode, scalar_moment_N_m, ampStyle):
+    network_code="XX"
+    inputunits = "m/s"
+    if ampStyle == AMP_STYLE_DISP:
+        inputunits = "m"
+    distList = []
+    if model.distance['type'] > 0:
+        distList.append(distance['distance'])
+    elif model.distance['type'] == DIST_REGULAR:
+        for i in range(model.distance['num']):
+            distList.append(model.distance['min']+i*model.distance['delta'])
+    else:
+        distList = model.distance['distanceList']
+    fakeXml = None
+    for d in distList:
+        deg = str(round(d/111.19, 2)).replace('.','_')
+        station_code = deg
+        data = {
+          "netcode": network_code,
+          "stacode": station_code,
+          "loccode": loccode,
+          "bandcode": bandcode,
+          "gaincode": gaincode,
+          "now": datetime.utcnow(),
+          "start": "1900-01-01T00:00:00",
+          "lat": 0.0,
+          "lon": d,
+          "sitename": f"fake {d} deg",
+          "radialaz": model.distance['azimuth'],
+          "transverseaz": (model.distance['azimuth']+90) % 360,
+          "gain": 1/moment_scale_factor(scalar_moment_N_m),
+          "inputunits": inputunits,
+          "sps": model.frequency['nyquist']
+        }
+        if fakeXml is None:
+            fakeXml = emptyStationXML.format(**data).strip()
+        else:
+            lines = fakeXml.split('\n')
+            lines = lines[:-2]
+            currlines = emptyStationXML.format(**data).strip().split('\n')
+            currlines = currlines[6:]
+            lines.extend(currlines)
+            fakeXml = "\n".join(lines)
+    return fakeXml
